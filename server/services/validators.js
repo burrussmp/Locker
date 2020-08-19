a0b9e8f117e61f992b2ff4fc4d0e001d25f2466c
Matthew Burruss <burrussmatthew@gmail.com>
2020-08-18 22:46:20 -0400

    added comments to validator.js

--- server/services/validators.js
+++ server/services/validators.js
@@ -1,19 +1,39 @@
 "use strict";
-
 import StaticStrings from '../../config/StaticStrings';
 
+/**
+  * @desc Check if valid email
+  * @param String str - A email
+  * @return true if valid else false
+*/ 
 const isValidEmail = (str) => {
     return str.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i);
 };
 
+/**
+  * @desc Check if valid phone number
+  * @param String str - A phone number
+  * @return true if valid else false
+*/ 
 const isValidPhoneNumber = (str) => {
     return str.match(/^[2-9]\d{2}-\d{3}-\d{4}$/i);
 }
 
+/**
+  * @desc Check if valid username
+  * @param String str - A username
+  * @return true if valid else false
+*/ 
 const isValidUsername = (str) => {
     return str.match(/^\w+$/i)
 }
 
+/**
+  * @desc Check if valid password
+  * @param String password  - A potential password
+  * @param Bool   isNew     - Whether or not this is a new model or an update
+  * @return error message if error else undefined
+*/ 
 const isValidPassword = (password,isNew) => {
     if (!password && isNew){
       return StaticStrings.UserModelErrors.PasswordRequired;
