edc6ed4b7b02966c90a092c86ed2d6a0d6880cc0
Matthew Burruss <burrussmatthew@gmail.com>
2020-09-16 11:24:25 -0400

    ready to go

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@ import AuthTest from './auth.test';
 import PostTest from './post.test';
 import {drop_database} from './helper';
 
-(async()=>{
-    await drop_database()
-})()
+// (async()=>{
+//     await drop_database()
+// })()
 
 
-// describe('Auth Tests',AuthTest)
+describe('Auth Tests',AuthTest)
 // describe('User Tests',UserTest)
 // describe('Post Test',PostTest)
