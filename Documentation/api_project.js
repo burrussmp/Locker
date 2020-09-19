582b0e77f2b7cf10718dbf32eb41826b2bf80d55
Matthew Burruss <burrussmatthew@gmail.com>
2020-09-18 23:51:12 -0400

    fixed major issue in documentation

--- Documentation/api_project.js
+++ Documentation/api_project.js
@@ -49,7 +49,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-19T03:48:15.578Z",
+    "time": "2020-09-19T03:49:08.132Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
