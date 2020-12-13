47ab92de9627907017a8adf4f6a611794a6fa948
Matthew Burruss <burrussmatthew@gmail.com>
2020-12-13 00:53:36 -0500

    made stage and dev env separate

--- config/config.js
+++ config/config.js
@@ -8,8 +8,34 @@ const getMongodbURI = () => {
     return process.env.MONGODB_URI || process.env.MONGO_HOST ||'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + `/${process.env.MONGO_TEST_DB_NAME}`;
   } else if (process.env.NODE_ENV == 'production') {
     throw Error('Production DB not implemented');
+  } else if (process.env.NODE_ENV == 'stage') {
+    return `mongodb+srv://MatthewBurruss:${process.env.MONGO_DEV_PASSWORD}@devopenmarket.mhwca.mongodb.net/${process.env.MONGO_DEV_DB_NAME}?retryWrites=true&w=majority`;
   } else {
     throw Error(`NODE_ENV set to ${process.env.NODE_ENV}: Invalid must be development, test, or production`);
+  }
+};
+
+const getIPAddress = () => {
+  if (process.env.NODE_ENV == 'test') {
+    return process.env.TEST_IP_ADDRESS;
+  } else if (process.env.NODE_ENV == 'development') {
+    return process.env.DEV_IP_ADDRESS;
+  } else if (process.env.NODE_ENV == 'stage') {
+    return process.env.STAGE_IP_ADDRESS;
+  } else {
+    throw Error(`Environment ${process.env.NODE_ENV} is not supported.`);
+  }
+};
+
+const getPort = () => {
+  if (process.env.NODE_ENV == 'test') {
+    return process.env.TEST_PORT;
+  } else if (process.env.NODE_ENV == 'development') {
+    return process.env.DEV_PORT;
+  } else if (process.env.NODE_ENV == 'stage') {
+    return process.env.STAGE_PORT;
+  } else {
+    throw Error(`Environment ${process.env.NODE_ENV} is not supported.`);
   }
 };
 
@@ -36,15 +62,26 @@ const getAWSConfig = () => {
       aws_employee_pool_id: process.env.AWS_EMPLOYEE_POOL_ID_TEST,
       aws_employee_pool_client_id: process.env.AWS_EMPLOYEE_POOL_CLIENT_ID_TEST,
     };
+  } else if (process.env.NODE_ENV == 'stage') {
+    return {
+      region: process.env.AWS_STAGE_REGION,
+      aws_access_key: process.env.AWS_ACCESS_KEY_ID_STAGE,
+      aws_secret: process.env.AWS_SECRET_ACCESS_KEY_STAGE,
+      aws_bucket_name: process.env.AWS_S3_TEST_BUCKET_STAGE,
+      aws_user_pool_id: process.env.AWS_USER_POOL_ID_STAGE,
+      aws_user_pool_client_id: process.env.AWS_USER_POOL_CLIENT_ID_STAGE,
+      aws_employee_pool_id: process.env.AWS_EMPLOYEE_POOL_ID_STAGE,
+      aws_employee_pool_client_id: process.env.AWS_EMPLOYEE_POOL_CLIENT_ID_STAGE,
+    };
   } else {
-    throw Error(`NODE_ENV set to ${process.env.NODE_ENV}: Invalid must be development, test, or production`);
+    throw Error(`NODE_ENV set to ${process.env.NODE_ENV}: Invalid must be development, test, or stage`);
   }
 };
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  address: process.env.NODE_ENV == 'test' ? '127.0.0.1' : process.env.IP_ADDRESS,
+  env: process.env.NODE_ENV,
+  port: getPort(),
+  address: getIPAddress(),
   mongoUri: getMongodbURI(),
   aws_config: getAWSConfig(),
 };
